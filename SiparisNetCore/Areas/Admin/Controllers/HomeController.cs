302ec9f7db807e68151c8daa75552d80ac2c70ea
cihancifci <50769743+cihancifci@users.noreply.github.com>
2021-08-24 21:17:58 +0300

    Ürün ekleme işlemleri yapıldı

--- SiparisNetCore/Areas/Admin/Controllers/HomeController.cs
+++ SiparisNetCore/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SiparisNetCore.Entities;
 using SiparisNetCore.Interfaces;
+using SiparisNetCore.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,6 +24,37 @@ namespace SiparisNetCore.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View(_urunRepository.GetirHepsi());
+        }
+        public IActionResult Ekle()
+        {
+            return View(new UrunEkleModel());
+        }
+
+        [HttpPost]
+        public IActionResult Ekle(UrunEkleModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Urun urun = new Urun();
+                if (model.resim != null)
+                {
+                    var uzanti = Path.GetExtension(model.resim.FileName);
+                    var yeniResimAd = Guid.NewGuid() + uzanti;
+                    var yuklenecekYer = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/img/" +yeniResimAd);
+
+                    var stream = new FileStream(yuklenecekYer,FileMode.Create);
+                    model.resim.CopyTo(stream);
+
+                    urun.Resim = yeniResimAd;
+                }
+                
+                urun.Ad = model.ad;
+                urun.Fiyat = model.fiyat;
+                _urunRepository.Ekle(urun);
+
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
+            }
+            return View();
         }
     }
 }
---
+++ SiparisNetCore/Models/UrunEkleModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiparisNetCore.Models
+{
+    public class UrunEkleModel
+    {
+        [Required(ErrorMessage ="Ad alanı boş girilemez!")]
+        public string ad { get; set; }
+        [Range(1, (double)decimal.MaxValue, ErrorMessage = "Fiyat 0 dan yüksek olmalıdır!")]
+        public decimal fiyat { get; set; }
+        public IFormFile resim { get; set; }
+    }
+}
